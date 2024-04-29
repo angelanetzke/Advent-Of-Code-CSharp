29acaa75c0bd1a85472bec6c9f0b20f7569e956d
Angela Netzke <angela.netzke@gmail.com>
2024-04-28 19:06:23 -0700

    Add 2016, Day 12

---
+++ 2016/Day12/Program.cs
@@ -0,0 +1,71 @@
+﻿Console.WriteLine($"Part 1: {Part1()}");
+Console.WriteLine($"Part 2: {Part2()}");
+
+static long Part1()
+{
+	long a = 1;
+	long b = 1;
+	long c = 0;
+	long d = 26;
+	while (d != 0)
+	{
+		c = a;
+		while (b != 0)
+		{			
+			a++;
+			b--;
+		}
+		b = c;
+		d--;
+	}
+	c = 19;
+	while (c != 0)
+	{
+		d = 14;
+		while (d != 0)
+		{
+			a++;
+			d--;
+		}
+		c--;
+	}
+	return a;
+}
+
+static long Part2()
+{
+	long a = 1;
+	long b = 1;
+	long c = 1;
+	long d = 26;
+	c = 7;
+	while (c != 0)
+	{
+		d++;
+		c--;
+	}
+	while (d != 0)
+	{
+		c = a;
+		while (b != 0)
+		{			
+			a++;
+			b--;
+		}
+		b = c;
+		d--;
+	}
+	c = 19;
+	while (c != 0)
+	{
+		d = 14;
+		while (d != 0)
+		{
+			a++;
+			d--;
+		}
+		c--;
+	}
+	return a;
+}
+
