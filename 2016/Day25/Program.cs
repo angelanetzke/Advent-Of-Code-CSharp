b3d812f3ab066b83fcf07bade0b2c1255e1df388
Angela Netzke <angela.netzke@gmail.com>
2024-05-04 18:31:18 -0700

    Add 2016, Day 25

---
+++ 2016/Day25/Program.cs
@@ -0,0 +1,7 @@
+﻿string[] allLines = File.ReadAllLines("input.txt");
+int number1 = int.Parse(allLines[1].Split(' ')[1]);
+int number2 = int.Parse(allLines[2].Split(' ')[1]);
+int product = number1 * number2;
+int sum = int.Parse("101010101010", System.Globalization.NumberStyles.BinaryNumber);
+int result = sum - product;
+Console.WriteLine($"Part 1: {result}");
