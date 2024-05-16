bf969f6430830cd638b4809bfc567578c9c12947
Angela Netzke <angela.netzke@gmail.com>
2024-05-16 10:23:43 -0700

    Add 2015, Day 25

---
+++ 2015/Day25/Program.cs
@@ -0,0 +1,21 @@
+﻿string input = File.ReadAllLines("input.txt")[0];
+int endRow = int.Parse(input.Split(' ')[16].Trim(','));
+int endColumn = int.Parse(input.Split(' ')[18].Trim('.'));
+int row = 1;
+int column = 1;
+long value = 20151125;
+while (row != endRow || column != endColumn)
+{
+	value = (value * 252533) % 33554393;
+	if (row == 1)
+	{
+		row = column + 1;
+		column = 1;
+	}
+	else
+	{
+		row--;
+		column++;
+	}
+}
+Console.WriteLine($"Part 1: {value}");
